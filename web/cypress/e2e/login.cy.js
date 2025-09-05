fc922221d2804930a8cf8729d5a68fe2388e4d52
Daniel Vilela <dvilelaportela@gmail.com>
2025-09-05 01:05:21 -0300

    feat: add cy.login

---
+++ web/cypress/e2e/login.cy.js
@@ -0,0 +1,14 @@
+describe('Login', () => {
+  it('Log Sucess', () => {
+    cy.openBrowser()
+
+    cy.submitLogin('papito@webdojo.com', 'katana123')
+
+    cy.contains('button', 'Entrar').click()
+
+    cy.get('[data-cy="welcome-message"]')
+      .should('be.visible'
+      .padEnd('have.text', 'Olá QA, esse é o seu Dojo para aprender Automação de Testes.')
+      )
+  })
+})
