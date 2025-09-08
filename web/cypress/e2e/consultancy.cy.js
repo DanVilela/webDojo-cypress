6423dc274b891addace608f6d645174fd4be93d2
Daniel Vilela <dvilelaportela@gmail.com>
2025-09-08 00:01:43 -0300

    feat: add check all checkbox in loop

--- web/cypress/e2e/consultancy.cy.js
+++ web/cypress/e2e/consultancy.cy.js
@@ -5,23 +5,42 @@ describe('Consulting Form', () => {
 
     cy.goToSteps('Formulários', 'Consultoria')
 
-    cy.get('input[placeholder="Digite seu nome completo"]')
-    .type('Teste Name')
-    cy.get('input[placeholder="Digite seu email"]')
-    .type('teste@email.com')
+    cy.get('input[placeholder="Digite seu nome completo"]').type('Teste Name')
+    cy.get('input[placeholder="Digite seu email"]').type('teste@email.com')
     cy.get('input[placeholder="(00) 00000-0000"]')
-    .type('21 99999-9999')
-    .should('have.value','(21) 99999-9999')
+      .type('21 99999-9999')
+      .should('have.value', '(21) 99999-9999')
 
     cy.get('#consultancyType').select('Individual')
 
     cy.contains('label', 'Pessoa Física')
-    .find('input')
-    .check()
-    .should('be.checked')
+      .find('input')
+      .click()
+      .should('be.checked')
 
     cy.contains('label', 'Pessoa Jurídica')
-    .find('input')
-    .should('be.not.checked')
+      .find('input')
+      .should('be.not.checked')
+
+    cy.contains('label', 'CPF')
+      .parent()
+      .find('input')
+      .type('16184318039')
+      .should('have.value', '161.843.180-39')
+
+    const discoveryChannels = [
+      'Instagram',
+      'LinkedIn',
+      'Udemy',
+      'Youtube',
+      'Indicação de Amigo',
+    ]
+
+    discoveryChannels.forEach((channel) => {
+      cy.contains('label', channel)
+      .find('input')
+      .check()
+      .should('be.checked')
+    })
   })
 })
