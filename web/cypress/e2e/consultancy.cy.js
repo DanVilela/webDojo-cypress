533d28041819c4330f585f947c7716f0725349fb
Daniel Vilela <dvilelaportela@gmail.com>
2025-09-07 23:23:29 -0300

    feat: add validation name and email using placeholder

--- web/cypress/e2e/consultancy.cy.js
+++ web/cypress/e2e/consultancy.cy.js
@@ -2,6 +2,12 @@ describe('Consulting Form', () => {
   it('Must request individual consultancy', () => {
     cy.openBrowser()
     cy.submitLogin('papito@webdojo.com', 'katana123')
+    
     cy.goToSteps('Formulários', 'Consultoria')
+
+    cy.get('input[placeholder="Digite seu nome completo"]')
+    .type('Teste Name')
+    cy.get('input[placeholder="Digite seu email"]')
+    .type('teste@email.com')
   })
 })
