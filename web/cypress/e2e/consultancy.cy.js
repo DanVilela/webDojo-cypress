de366c1dd23da20fa1c6504be1d59b10773b3da3
Daniel Vilela <dvilelaportela@gmail.com>
2025-09-07 23:26:48 -0300

    feat: add validation celphone using placeholder and value validation

--- web/cypress/e2e/consultancy.cy.js
+++ web/cypress/e2e/consultancy.cy.js
@@ -2,12 +2,15 @@ describe('Consulting Form', () => {
   it('Must request individual consultancy', () => {
     cy.openBrowser()
     cy.submitLogin('papito@webdojo.com', 'katana123')
-    
+
     cy.goToSteps('Formulários', 'Consultoria')
 
     cy.get('input[placeholder="Digite seu nome completo"]')
     .type('Teste Name')
     cy.get('input[placeholder="Digite seu email"]')
     .type('teste@email.com')
+    cy.get('input[placeholder="(00) 00000-0000"]')
+    .type('21 99999-9999')
+    .should('have.value','(21) 99999-9999')
   })
 })
