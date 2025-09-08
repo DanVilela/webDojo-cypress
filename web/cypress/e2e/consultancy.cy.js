122516e161d2802cf272ee136a489dc31f353b2c
Daniel Vilela <dvilelaportela@gmail.com>
2025-09-07 22:23:15 -0300

    fix: inprove consultancy step

--- web/cypress/e2e/consultancy.cy.js
+++ web/cypress/e2e/consultancy.cy.js
@@ -1,10 +1,7 @@
-describe("Consulting Form", () => {
-  it("Must request individual consultancy", () => {
-    cy.openBrowser();
-    cy.submitLogin("papito@webdojo.com", "katana123");
-
-    cy.contains("button", "Formulários").should("be.visible").click();
-
-    cy.contains("h1", "onsultoria").should("be.visible");
-  });
-});
+describe('Consulting Form', () => {
+  it('Must request individual consultancy', () => {
+    cy.openBrowser()
+    cy.submitLogin('papito@webdojo.com', 'katana123')
+    cy.goToSteps('Formulários', 'Consultoria')
+  })
+})
--- web/cypress/support/commands.js
+++ web/cypress/support/commands.js
@@ -24,19 +24,24 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
-Cypress.Commands.add("openBrowser", () => {
-  cy.viewport(1440, 900);
-  cy.visit("http://localhost:3000");
+Cypress.Commands.add('openBrowser', () => {
+  cy.viewport(1440, 900)
+  cy.visit('http://localhost:3000')
 });
 
-Cypress.Commands.add("submitLogin", (email, password) => {
-  cy.get("#email").type(email);
-  cy.get("#password").type(password);
+Cypress.Commands.add('submitLogin', (email, password) => {
+  cy.get('#email').type(email)
+  cy.get('#password').type(password)
   cy.contains('button', 'Entrar').click()
 });
 
-Cypress.Commands.add("start", (email, password) => {
-  cy.get("#email").type(email);
-  cy.get("#password").type(password);
+Cypress.Commands.add('start', (email, password) => {
+  cy.get('#email').type(email)
+  cy.get('#password').type(password)
 });
 
+Cypress.Commands.add('goToSteps', (buttonName, pageTitle) => {
+  cy.contains('button', buttonName).should('be.visible').click();
+
+  cy.contains('h1', pageTitle).should('be.visible')
+});
